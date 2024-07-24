0620cc087623e77d2730f899b916d3e66202e2cb
isa <isaurini1902@gmail.com>
2024-07-24 14:07:09 -0600

    manejo de erorr es

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -2,12 +2,20 @@ import { heroes } from "../data/heroes"
 
 export const callbackComponent =(elements )=>{
     const id ='5d86371f2343e37870b91ef1';
-    findHeroe(id, (idk)=>{
-        elements.innerHTML=idk.name;
+    findHeroe(id, (error,idk)=>{
+        if(error){
+           elements.innerHTML=error;
+            return;
+        }
+        elements.innerHTML=idk.name ;
     });
 }
 
 const findHeroe = (id, callback) => {
    const hero= heroes.find(hero=>hero.id===id);
-    callback(hero);
+   if(!hero){
+        callback(`Hero with id ${id} not found`);
+        return;
+   }
+    callback(null,hero);
 }
